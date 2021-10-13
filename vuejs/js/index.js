d088059ae7647126cfad1a124e6fd2d6750e9d55
Rudy <rudy.amri88@gmail.com>
2021-10-13 21:58:57 +0800

    added vuejs

---
+++ vuejs/js/index.js
@@ -0,0 +1,49 @@
+const app1 = new Vue({
+    el: "#app1",
+    data: {
+        message: "Hello Vue"
+    }
+});
+
+const app2 = new Vue({
+    el: '#app2',
+    data: {
+        message: 'Loaded on ' + new Date().toLocaleString(),
+        linkUrl: 'https://bbc.in/2LAnJs8',
+        linkText: 'Google is down!'
+    }
+});
+
+const app3 = new Vue({
+    el: '#app3',
+    data: {
+        showIt: false
+    },
+    methods: {
+        toggleMsg: function() {
+            if(this.showIt) {
+                this.showIt = false
+            } else {
+                this.showIt = true
+            }
+        }
+    }
+});
+
+const app4 = new Vue({
+    el: "#app4",
+    data: {
+        theList: [
+            { text: "Learn HTML" },
+            { text: "Learn CSS" },
+            { text: "Learn Javascript" },
+        ]
+    }
+});
+
+const app5 = new Vue({
+    el: '#app5',
+    data: {
+        message: "Hello Word!"
+    }
+})
